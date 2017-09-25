5593914857b333e51196b17ab962c980408432be
Rahnuma Nurain Maria <Rahnuma Nurain Maria>
2017-09-25 10:27:41 +0600

    test method created.

--- FindGene/Part1.java
+++ FindGene/Part1.java
@@ -1,33 +1,14 @@
 
-/**
- * Write a description of class Part1 here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class Part1
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Part1
-     */
-    public Part1()
+    
+    
+    
+    
+    void test()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
